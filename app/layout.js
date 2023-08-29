80b7c096e33e1b5c1190c0c6cb490e0b76db650a
Hiranmoy <hiranmoyhs380@gmail.com>
2023-08-29 21:57:36 +0530

    second commitss

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className=''>
+    <html lang="en" className='dark'>
 
       <body className={inter.className}>
         <ClientOnly>
